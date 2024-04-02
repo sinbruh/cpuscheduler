aabd2a0749047fda1a7d3b420524539a128c47c5
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-04-02 19:14:05 +0200

    Added JavaDoc and comments to fcfsAlgo method in Scheduler class

--- src/main/java/no/ntnu/opsys/group2/Main.java
+++ src/main/java/no/ntnu/opsys/group2/Main.java
@@ -1,6 +1,23 @@
 package no.ntnu.opsys.group2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
+    List<Process> processes = new ArrayList<>();
+    Process p1 = new Process(1, 4, 5);
+    Process p2 = new Process(2, 6, 4);
+    Process p3 = new Process(3, 0, 3);
+    Process p4 = new Process(4, 6, 2);
+    Process p5 = new Process(5, 5, 4);
+    processes.add(p1);
+    processes.add(p2);
+    processes.add(p3);
+    processes.add(p4);
+    processes.add(p5);
+    Scheduler sc = new Scheduler();
+    System.out.println(sc.fcfsAlgo(processes).get(0));
+    System.out.println(sc.fcfsAlgo(processes).get(1));
   }
 }
--- src/main/java/no/ntnu/opsys/group2/Scheduler.java
+++ src/main/java/no/ntnu/opsys/group2/Scheduler.java
@@ -1,14 +1,29 @@
 package no.ntnu.opsys.group2;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Scheduler {
-  public double getFcfsAvgWaitingTime(List<Process> processes) {
+  /**
+   * Returns a list containing the Average Waiting Time and Average Turn Around Time respectively
+   * from a specified list of processes using First Come First Serve algorithm.
+   * 
+   * @param processes A specifed list of processes
+   * @return A list containing the Average Waiting Time and Average Turn Around Time respectively
+   */
+  public List<Double> fcfsAlgo(List<Process> processes) {
+    // Defines variable to store results
+    List<Double> results = new ArrayList<>();
+
+    // Defines ticks
     int t = 0;
+    // Defines variables to store total results
+    double totalTurnAroundTime = 0.0;
     double totalWaitingTime = 0.0;
 
+    // Sorts processes by arrival time
     processes = this.sortByArrivalTime(processes);
 
     for (Process process : processes) {
@@ -16,8 +31,11 @@ public class Scheduler {
       int waitingTime = 0;
       boolean finished = false;
       while (!finished) {
+        // Checks if the process has arrived at the current tick
         if (process.getArrivalTime() <= t) {
+          // Jumps to the tick where the burst time is finished
           t += process.getBurstTime();
+          // Calculates Turn Around Time and Waiting Time for the current process
           turnAroundTime = this.calculateTurnAroundTime(t, process.getArrivalTime());
           waitingTime = this.calculateWaitingTime(turnAroundTime, process.getBurstTime());
           finished = true;
@@ -25,9 +43,15 @@ public class Scheduler {
           t++;
         }
       }
+      // Adds the Turn Around Time and Waiting time for the current process to the total results
+      totalTurnAroundTime += turnAroundTime;
       totalWaitingTime += waitingTime;
     }
-    return totalWaitingTime / processes.size();
+    // Adds the average of the total results to the result list
+    results.add(totalTurnAroundTime / processes.size());
+    results.add(totalWaitingTime / processes.size());
+
+    return results;
   }
 
   private int calculateTurnAroundTime(int completionTime, int arrivalTime) {
